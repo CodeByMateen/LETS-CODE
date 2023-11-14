d5ba4d310647c576c963669f84206cbc0b760895
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-11-14 20:21:26 +0500

    Add files via upload

--- LeetCode/Blind 75/array_twoSum.py
+++ LeetCode/Blind 75/array_twoSum.py
@@ -1,3 +1,4 @@
+# Solution 1
 def twoSum(nums, target):
     num_set = set()
 
@@ -8,3 +9,20 @@ def twoSum(nums, target):
         num_set.add(num)
 
 print(twoSum([2,7,11,15],9))
+
+# Solution 2
+def twoSum(nums, target):
+    numsSort = sorted(nums)
+    left, right = 0, len(numsSort)-1
+    while left<right:
+        complement = numsSort[left]+numsSort[right]
+        if complement==target:
+            return numsSort[left],numsSort[right]
+        elif complement<target:
+            left+=1
+        else:
+            right-=1
+    return None
+
+
+print(twoSum([2,7,11,15], 9))
