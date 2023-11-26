2fe6cbd416b6fb54ebb3d57e9e708a62073e0d45
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-11-26 22:03:13 +0500

    Add files via upload

---
+++ LeetCode/Blind 75/03_array_duplicate_elements_in_list.py
@@ -0,0 +1,18 @@
+# Solution 1 self
+nums = [1,1,1,3,3,4,3,2,4,2]
+numnew=[]
+for i in nums:
+    if i in numnew:
+        print("duplicate found of", i)
+    else:
+        numnew.append(i)
+        
+# Solution 2 
+
+nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+num_set = set()
+for i in nums:
+    if i in num_set:
+        print("Duplicate found of", i)
+    else:
+        num_set.add(i)
