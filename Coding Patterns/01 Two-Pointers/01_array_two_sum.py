5513c129220f244c9ab38df36575a00b7c706abd
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2025-05-02 05:23:24 +0500

    Update 01_array_two_sum.py

--- Coding Patterns/01 Two-Pointers/01_array_two_sum.py
+++ Coding Patterns/01 Two-Pointers/01_array_two_sum.py
@@ -1,1 +1,17 @@
-# 
+l = [2, 3, 5, 7, 8, 9]
+target = 10
+
+# for i in range(len(l)):
+#     for j in range(i + 1, len(l)):
+#         print(l[i]+l[j])
+
+# nums = [2, 7, 11, 15]
+# for i in range(len(nums)):
+#     for j in range(i + 1, len(nums)):
+#         print(f"Checking: {nums[i]} + {nums[j]} = {nums[i] + nums[j]}")
+
+for i in range(len(l)):
+    for j in range(i + 1, len(l)):
+        if l[i] + l[j] == target:
+            print([i, j])
+            
